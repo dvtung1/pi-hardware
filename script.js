d2dfb027a06e630e692cc2d2a27269c2a9c14957
Tung Dao <dvtung98@gmail.com>
2019-03-16 01:07:51 +0000

    send http post to the server when buttons are pressed, use temporary dynamic Backend URL

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ const five = require("johnny-five");
 const board = new five.Board({
 	io: new Raspi()
 });
+const axios = require("axios");
+//temp dynamic backend url
+//after deploy, will get static url
+const BACKEND_URL = "http://2cd09f63.ngrok.io/api/rpi/rating";
 
 board.on("ready", () => {
-	//set excellent button at pin 7
-	buttonExcellent = new five.Button("P1-7");
-	//set satisfactory button at pin 9
-	buttonSatisfactory = new five.Button("P1-9");
-	//set poor button at pin 11
-	buttonPoor = new five.Button("P1-11");
+	//set excellent button at pin 11
+	buttonExcellent = new five.Button("P1-11");
+	//set satisfactory button at pin 13
+	buttonSatisfactory = new five.Button("P1-13");
+	//set poor button at pin 15
+	buttonPoor = new five.Button("P1-15");
 
-	//when excellent button is pressed
 	buttonExcellent.on("down", function() {
-		console.log("excellent clicked");
+		sendRatingBackend("excellent");
 	});
-	buttonExcellent.on("up", function() {
-		console.log("excellent release");
+	buttonSatisfactory.on("down", function() {
+		sendRatingBackend("satisfactory");
 	});
+	buttonPoor.on("down", function() {
+		sendRatingBackend("poor");
+	});
+	// buttonExcellent.on("up", function() {
+	// 	console.log("excellent release");
+	// });
 });
+
+sendRatingBackend = rating => {
+	axios
+		.post(BACKEND_URL, { rating: rating })
+		.then(res => {
+			console.log("Server: " + res.data.message);
+		})
+		.catch(err => {
+			console.log(err.response.data.message);
+		});
+};
