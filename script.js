8117a43c76277697727fb8c3b533f8c029b4113b
Tung Dao <dvtung98@gmail.com>
2019-03-22 06:53:49 +0000

    add a temporary sound effect for piezo buzzer

--- script.js
+++ script.js
@@ -10,26 +10,65 @@ const BACKEND_URL = "https://purfectdining-server.herokuapp.com/api/rpi";
 //this rpi will be set up at Ford Dining Court
 const PLACE = "Ford";
 
+const SONG = {
+	song: [
+		["C4", 1 / 4],
+		["D4", 1 / 4],
+		["F4", 1 / 4],
+		["D4", 1 / 4],
+		["A4", 1 / 4],
+		[null, 1 / 4],
+		["A4", 1],
+		["G4", 1],
+		[null, 1 / 2],
+		["C4", 1 / 4],
+		["D4", 1 / 4],
+		["F4", 1 / 4],
+		["D4", 1 / 4],
+		["G4", 1 / 4],
+		[null, 1 / 4],
+		["G4", 1],
+		["F4", 1],
+		[null, 1 / 2]
+	],
+	tempo: 100
+};
+
 board.on("ready", () => {
 	//set excellent button at pin 11
-	buttonExcellent = new five.Button("P1-11");
+	var buttonExcellent = new five.Button("P1-11");
 	//set satisfactory button at pin 13
-	buttonSatisfactory = new five.Button("P1-13");
+	var buttonSatisfactory = new five.Button("P1-13");
 	//set poor button at pin 15
-	buttonPoor = new five.Button("P1-15");
+	var buttonPoor = new five.Button("P1-15");
+
+	var piezo = new five.Piezo("P1-32");
+
+	board.repl.inject({
+		piezo: piezo
+	});
 
 	buttonExcellent.on("down", function() {
+		console.log("Sending..");
 		sendRatingBackend("excellent");
 	});
 	buttonSatisfactory.on("down", function() {
+		console.log("Sending..");
 		sendRatingBackend("satisfactory");
 	});
 	buttonPoor.on("down", function() {
+		console.log("Sending..");
 		sendRatingBackend("poor");
 	});
-	// buttonExcellent.on("up", function() {
-	// 	console.log("excellent release");
-	// });
+	buttonExcellent.on("up", function() {
+		piezo.play(SONG);
+	});
+	buttonSatisfactory.on("up", function() {
+		piezo.play(SONG);
+	});
+	buttonPoor.on("up", function() {
+		piezo.play(SONG);
+	});
 });
 
 sendRatingBackend = rating => {
